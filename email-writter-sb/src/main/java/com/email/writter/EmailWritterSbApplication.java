9b19e3a3e289344ae36a1b9b7b62d7e022582a04
ujjwal0 <83915211+ujjwal0@users.noreply.github.com>
2025-11-16 17:11:44 +0530

    first commit

---
+++ email-writter-sb/src/main/java/com/email/writter/EmailWritterSbApplication.java
@@ -0,0 +1,13 @@
+package com.email.writter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWritterSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWritterSbApplication.class, args);
+	}
+
+}
---
+++ email-writter-sb/src/test/java/com/email/writter/EmailWritterSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.writter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWritterSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
