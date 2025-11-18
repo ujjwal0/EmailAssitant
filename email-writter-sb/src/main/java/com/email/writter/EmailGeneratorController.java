e3bea2026af0d2cdd20ede7d704d6923a80baa61
ujjwal0 <83915211+ujjwal0@users.noreply.github.com>
2025-11-19 00:27:19 +0530

    added some buisness logig in service layer

--- email-writter-sb/src/main/java/com/email/writter/EmailGeneratorController.java
+++ email-writter-sb/src/main/java/com/email/writter/EmailGeneratorController.java
@@ -3,6 +3,9 @@ package com.email.writter;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class EmailGeneratorController {
     private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response =emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
 }
--- email-writter-sb/src/main/java/com/email/writter/EmailGeneratorService.java
+++ email-writter-sb/src/main/java/com/email/writter/EmailGeneratorService.java
@@ -1,7 +1,48 @@
 package com.email.writter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class EmailGeneratorService {
+    private final WebClient webClient;
+    private final String apiKey;
+
+    public  EmailGeneratorService(WebClient.Builder webClientBuilder,@Value("${gemini.api.url}") String baseURL,@Value("${gemini.api.key}") String geminiAPIKEY){
+        this.apiKey=geminiAPIKEY;
+        this.webClient=webClientBuilder.baseUrl(baseURL).build();
+    }
+    public String generateEmailReply(EmailRequest emailRequest) {
+
+        //Buid Prompt
+        String prompt=buildPrompt(emailRequest);
+        //prepare Raw json
+        String requestBody=String.format("""
+                {
+                "contents": [
+                      {
+                        "parts": [
+                          {
+                            "text": "%s"
+                          }
+                        ]
+                      }
+                    ]
+                }
+                """,prompt);
+        //send Request
+        //Extract Response
+
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt= new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email:");
+        if(emailRequest.getTone()!=null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append("tone.");
+        }
+        prompt.append("Original email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
 }
